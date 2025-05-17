b2728c00aeef348c5fdefb83e4516596e088b169
NalchajyanHayk <hayk.nalchajyan@gmail.com>
2025-05-17 19:11:52 +0400

    Added new update functions for dim tables

--- pipeline_dimensional_data/flow.py
+++ pipeline_dimensional_data/flow.py
@@ -1,27 +1,50 @@
-from pipeline_dimensional_data.tasks import load_all_staging_tables, create_staging_raw_tables
+from pipeline_dimensional_data.tasks import (
+    load_all_staging_tables,
+    create_staging_raw_tables,
+    update_dim_categories,
+    update_dim_customers,
+    update_dim_employees,
+    update_dim_suppliers,
+    update_dim_products,
+    update_dim_region,
+    update_dim_shippers,
+    update_dim_territories,
+    update_fact_orders 
+)
+
 from loguru import logger
-from pipeline_dimensional_data.tasks import update_dim_categories
 
 class DimensionalDataFlow:
     def exec(self, start_date, end_date):
         logger.info("ETL pipeline started.")
-        logger.info("Ensuring staging tables exist...")
-
-        logger.info("Ensuring staging tables exist...")
         create_staging_raw_tables()
-        logger.info("Staging tables created.")
-        
-        # Load all staging tables at once
+
         result = load_all_staging_tables("raw_data_source.xlsx")
         if not result["success"]:
             logger.error("Staging load failed.")
             return
 
-        logger.info("Finished staging load. Proceeding to dimension updates...")
+        logger.info("Staging load completed. Updating dimensions...")
 
-        result = update_dim_categories()
-        if not result["success"]:
-            logger.error("Aborting: DimCategories update failed.")
-            return
+        for update_func in [
+            update_dim_categories,
+            update_dim_customers,
+            # update_dim_employees,
+            update_dim_suppliers,
+            update_dim_products,
+            update_dim_region,
+            update_dim_shippers,
+            update_dim_territories
+        ]:
+            result = update_func()
+            if not result["success"]:
+                logger.error("Aborting: {} failed.".format(update_func.__name__))
+                return
+
+        # logger.info("Dimension updates complete. Updating fact table...")
+        # result = update_fact_orders()
+        # if not result["success"]:
+        #     logger.error("FactOrders update failed.")
+        #     return
 
         logger.info("ETL pipeline completed.")
