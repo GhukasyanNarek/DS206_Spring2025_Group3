b881df305b26ab1fa2db23f92586e4c4840ca081
Narek Ghukasyan <narek_ghukasyan@edu.aua.am>
2025-05-17 15:46:51 +0400

    Added __init__ for DimensionalDataFlow, to have unique uuid for every execution

--- pipeline_dimensional_data/flow.py
+++ pipeline_dimensional_data/flow.py
@@ -1,8 +1,15 @@
 from pipeline_dimensional_data.tasks import load_all_staging_tables, create_staging_raw_tables
 from loguru import logger
 from pipeline_dimensional_data.tasks import update_dim_categories
+from utils import generate_execution_id
+from general_logging import logger as configured_logger
+
 
 class DimensionalDataFlow:
+    def __init__(self):
+        self.execution_id = generate_execution_id()
+        configured_logger.bind(execution_id=self.execution_id)
+
     def exec(self, start_date, end_date):
         logger.info("ETL pipeline started.")
         logger.info("Ensuring staging tables exist...")
