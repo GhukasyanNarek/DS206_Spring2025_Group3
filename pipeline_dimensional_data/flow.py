7c671755ed88e5dda127418266a1dbba1a4283a4
Narek Ghukasyan <narek_ghukasyan@edu.aua.am>
2025-05-18 05:04:21 +0400

    Finalized the overall flow

--- pipeline_dimensional_data/flow.py
+++ pipeline_dimensional_data/flow.py
@@ -1,6 +1,9 @@
 from pipeline_dimensional_data.tasks import (
-    update_dim_categories, update_dim_employees, update_dim_sor,
-    load_all_staging_tables, create_staging_raw_tables, update_dim_regions, update_dim_customers)
+    update_all_dimensions,
+    update_dim_sor,
+    load_all_staging_tables,
+    create_staging_raw_tables
+)
 from utils import generate_execution_id
 from general_logging import logger as configured_logger
 from loguru import logger
@@ -8,42 +11,28 @@ from loguru import logger
 class DimensionalDataFlow:
     def __init__(self):
         self.execution_id = generate_execution_id()
-        configured_logger.bind(execution_id=self.execution_id)
+        self.logger = configured_logger.bind(execution_id=self.execution_id)
 
     def exec(self, start_date, end_date, create_staging=False):
         logger.info("ETL pipeline started.")
+
         if create_staging:
             logger.info("Creating staging tables...")
             create_staging_raw_tables()
             logger.info("Staging tables created.")
+
             logger.info("Populating staging tables from raw Excel...")
             load_all_staging_tables("raw_data_source.xlsx")
-        else:
-            logger.info("Skipping staging table creation and load (no --reset passed).")
+            logger.info("Staging tables populated.")
 
         logger.info("Updating Dim_SOR...")
         if not update_dim_sor()["success"]:
             logger.error("Aborting: update_dim_sor failed.")
             return
 
-        logger.info("Updating DimCategories...")
-        if not update_dim_categories()["success"]:
-            logger.error("Aborting: update_dim_categories failed.")
+        logger.info("Starting update of all dimensional tables...")
+        if not update_all_dimensions()["success"]:
+            logger.error("Aborting: update_all_dimensions failed.")
             return
 
-        logger.info("Updating DimEmployees...")
-        if not update_dim_employees()["success"]:
-            logger.error("Aborting: update_dim_employees failed.")
-            return
-        
-        logger.info("Updating DimRegions...")
-        if not update_dim_regions()["success"]:
-            logger.error("Aborting: update_dim_regions failed.")
-            return
-        
-        logger.info("Updating DimCustomers...")
-        if not update_dim_customers()["success"]:
-            logger.error("Aborting: update_dim_customers failed.")
-            return
-
-    logger.info("ETL pipeline completed.")
+        logger.info("ETL pipeline completed.")
